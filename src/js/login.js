d1e4804a929522f414c032456e2e1cda4b574598
jxLiu <2581545272@qq.com>
2022-09-29 16:17:32 +0800

    增加了登陆功能

---
+++ src/js/login.js
@@ -0,0 +1,41 @@
+let mysql = require('mysql');
+const { read } = require('original-fs');
+
+
+
+document.getElementById('login').onclick = function(){
+    let number = document.getElementById('user').value;
+    let password = document.getElementById('password').value;
+    let answer = document.getElementById('answer');
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'userdata'
+    });
+    connection.connect();
+    let sql = "select passwd from user where phone='" + number + "';";
+    connection.query(sql, function(err, result){
+        if(!err){
+            if(result[0]){
+                if(password == result[0]['passwd']){
+                    answer.innerHTML = '登陆成功';
+                    setTimeout(()=>{
+                        window.location.href='./index.html';
+                    }, 2000);
+                    return;
+                }else{
+                    answer.innerHTML = "密码错误";
+                    return;
+                }
+            }else{
+                answer.innerHTML = "用户不存在";
+                return;
+            }
+        }else{
+            console.log(err.message);
+            alert("读取数据库失败");
+            return;
+        }
+    })
+}
---
+++ src/js/register.js
@@ -0,0 +1,54 @@
+let mysql = require('mysql');
+
+
+document.getElementById('register').onclick = function(){
+    let phone = document.getElementById('uid').value;
+    let username = document.getElementById('user').value;
+    let password = document.getElementById('password').value;
+    let passwordAgain = document.getElementById('passwordAgain').value;
+    let answer = document.getElementById('answer');
+    if(!password || !passwordAgain || !phone || !username){
+        answer.innerHTML = "请输入完整信息";
+        return;
+    }
+    if(password != passwordAgain){
+        answer.innerHTML = "两次密码输入不一致";
+        return;
+    }
+
+
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'userdata'
+    });
+    connection.connect();
+    let sql = "select passwd from user where phone='" + phone + "';";
+
+    connection.query(sql, function(err, result){
+        if(!err){
+            if(result[0]){
+                answer.innerHTML = "用户已经存在";
+                return;
+            }else{
+                let sql = "insert into user(username, passwd, phone) values('" + username + "','" + password +"','" + phone + "');";
+                connection.query(sql, function(err){
+                    if(err){
+                        console.log(err.message);
+                        answer.innerHTML = "注册失败";
+                        return;
+                    }else{
+                        answer.innerHTML = "注册成功";
+                        setTimeout(()=>{
+                            window.location.href = './login.html';
+                        }, 2000);
+                        return;
+                    }
+                })
+            }
+        }else{
+            return alert('读取数据库失败');
+        }
+    })
+}

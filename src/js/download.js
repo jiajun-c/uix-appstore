dfa1235ead0deb08418fcdef8b3b202e9e58904b
jiajun-c <2966623227@qq.com>
2022-10-02 15:48:14 +0800

    实现查找软件包和来源并显示

--- src/js/download.js
+++ src/js/download.js
@@ -5,23 +5,26 @@ var child = require('child_process');
 
 window.onload = function () {
   const btn = document.querySelector("#btn");
-  const myData = document.querySelector("#my-data");
-
+  const myData = document.getElementById("res");
+  const input = document.getElementById('search_package')
   btn.onclick = function () {
     // 读取data.txt文件
 
-    fs.readFile("package.json", (err, data) => {
-      写入网页
-      myData.innerHTML = data;
-    })
-    child.exec('yay -Ss v2ray', function(err, data) {
+
+    console.log(input.nodeValue);
+    child.exec('yay -Ss '.concat(input.value), function(err, data) {
         // myData.innerHTML = data;
         console.log(data);
         fs.writeFile("data/temp", data, (err) => {
           console.log(err);
           exec("python -u src/backend/search_package.py");
         });
+        // fs.readFile("package.json", (err, data) => {
+        //   myData.innerHTML = data;
+        // })
+        fs.readFile("data/aur_packages.json",(err, data)=> {
+          myData.innerText = data;
+        } )
     })
   }
-
 }
